292b1c9ca78135c8b2cef1ce174a477dae1e8ecd
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-05-19 23:51:19 -0300

    Aprendendo sobre cache no langchain

--- integration_ia.py
+++ integration_ia.py
@@ -1,6 +1,10 @@
-import os
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI, Gemini
+from langchain_core.globals import set_llm_cache
+from langchain_community.cache import SQLiteCache
 from decouple import config
+
+
+set_llm_cache(SQLiteCache(database_path= 'django-questions.db'))
 
 question_user = input("Faça sua pergunta sobre Django:\n")
 
@@ -10,6 +14,7 @@ client = GoogleGenerativeAI(model="gemini-3.1-flash-lite",
 
 messages = [
     {"role": "system", "content": "Você é um profissional em django e django rest framework"},
+    {"role": "system", "content": "Responda somente perguntas relacionadas ao Framework Django"},
     {"role": "user", "content": f"{question_user}"}
 ]
 
