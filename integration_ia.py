bc0f2eeed89d6c01156c0227278e92a226604dc8
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-05-24 21:46:02 -0300

    Learning about PromptTemplates

--- integration_ia.py
+++ integration_ia.py
@@ -1,23 +1,29 @@
-from langchain_google_genai import GoogleGenerativeAI, Gemini
+from langchain_google_genai import GoogleGenerativeAI
 from langchain_core.globals import set_llm_cache
+from langchain_core.prompts import PromptTemplate
 from langchain_community.cache import SQLiteCache
 from decouple import config
 
 
 set_llm_cache(SQLiteCache(database_path= 'django-questions.db'))
 
-question_user = input("Faça sua pergunta sobre Django:\n")
+#question_user = input("Faça sua pergunta sobre Django:\n")
 
 client = GoogleGenerativeAI(model="gemini-3.1-flash-lite",
-                             api_key = config("GOOGLE_API_KEY"))
+                             api_key = config("GOOGLE_API_KEY"),
+                             stream=True)
 
 
-messages = [
-    {"role": "system", "content": "Você é um profissional em django e django rest framework"},
-    {"role": "system", "content": "Responda somente perguntas relacionadas ao Framework Django"},
-    {"role": "user", "content": f"{question_user}"}
-]
+#template = "Exiba a diferença entre o framework {framework_1} e o framework {framework_2} para o codigo {codigo}" 
 
-response = client.invoke(messages)
+#prompt_template = PromptTemplate.from_template(template=template)
 
+prompt = PromptTemplate.template("Exiba a diferença entre o framework {framework_1} e o framework {framework_2} para o codigo {codigo}")
+
+# messages = [
+#     {"role": "system", "content": "Você é um profissional em django e django rest framework"},
+#     {"role": "user", "content": f"{question_user}"}
+# ]
+
+response = client.invoke()
 print(response)
