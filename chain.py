3f5b4e03434426f75cb7e69dc9b9adbaa9dd3bca
JoaoGFS <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-06-08 16:32:07 -0300

    Learning about simple chains

--- chain.py
+++ chain.py
@@ -8,7 +8,10 @@ model = ChatGoogleGenerativeAI(model ="gemini-3.1-flash-lite",
                              api_key = config("GOOGLE_API_KEY"))
 
 runnable_sequence = (PromptTemplate.from_template("Me fale as diferenças entre as linguagens: {linguagem_1} x {linguagem_2}") | model | StrOutputParser())
+#Para chains é necessario realizar uma tupla de sequencia. Por exemplo: Primeiro atribue o valor ao prompt_template, invoque o model e depois no response de o stroutput 
 
 response = runnable_sequence.invoke({"linguagem_1": "Javascript", 
                         "linguagem_2":"Golang"})
+
+#Para atribuir valores ao prompt template, será sempre com dicionarios
 print(response)
